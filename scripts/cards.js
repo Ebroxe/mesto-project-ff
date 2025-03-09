3ae8d2810cfd10891a03525f6cd31554cdc41b85
Daria Ermakova <erdarya89@gmail.com>
2025-03-10 01:25:16 +0700

    First commit

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Зеленые горы",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Темное озеро посреди зимнего леса",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Район, плотно застроенный серыми панельными домами",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Горная вершина, покрытая снегом",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Железная дорога",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Озеро под ледяным покровом",
     }
 ];
